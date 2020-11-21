3270223dc58292b58467218cefe8c507d24bf997
Gregers <gregers.boye@gmail.com>
2020-11-21 16:45:00 +0100

    Added GPIO-action to networkchecker

--- checkNetwork.py
+++ checkNetwork.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
-import os
-import time
+import RPi.GPIO as GPIO
 import urllib.request
 import time
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2,GPIO.OUT)
 try:
     while True:
+        GPIO.output(2,GPIO.LOW)
+        time.sleep(0.25)
         try:
             urllib.request.urlopen("http://www.google.com").close()
         except:
             print("Not Connected")
         else:
             print("Connected")
+            GPIO.output(2, GPIO.HIGH)
         time.sleep(1)
 except KeyboardInterrupt:
     print("Keyboard stopped the show")
-    
+    GPIO.cleanup()
