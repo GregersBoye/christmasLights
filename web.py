0199ec4845c786d092b804e961987b8985390f60
Gregers <gregers.boye@gmail.com>
2020-11-21 15:12:08 +0100

    Added state-route and reorganized code

--- web.py
+++ web.py
@@ -20,9 +20,9 @@ class SimpleHandler(BaseHTTPRequestHandler):
         GPIO.output(val, GPIO.HIGH)
 
     print("Listening on port 1224")
+    print("~~~~~~~~~")
     def do_GET(self):
         message  = ""
-        GPIO.output(14, 1)
         if "setlights" in self.path:
             settings = int(self.path.split("/")[2])
             state = "";
@@ -32,30 +32,38 @@ class SimpleHandler(BaseHTTPRequestHandler):
                 if settings&(2**i):
                     time.sleep(delay)
                     newState = GPIO.LOW
-                    result = "Turn on pin #"+str(i+1) + "(GPIO #" +str(val) +") value: "+ str(newState)
-                else:
-                    result = "Turn off pin #"+str(i+1) + "(GPIO #" + str(val) + ") value: "+ str(newState)
+                result = "Pin #"+str(i+1) + "\t(GPIO #" + str(val) + ") \tHIGH: "+ str(not newState)
                 print(result)
-                message  += result+ "<br />\r\n"
                 state = str(newState) + state
                 GPIO.output(val, newState)
             print("~~~~~~~~~~") 
-            
-            today = datetime.now()
-            fileformat  = today.strftime("%Y_%m_%d")+".log"
-            logFile = open("/var/log/christmas_server/"+fileformat, "a+")
-            timestamp = today.strftime("%H:%M:%S.%f")
-            print("New state: "+state)
-            logFile.write(timestamp+ ": Set lights to code "+state+"\r\n")
+            self.write_log(state)
+            self.show_status()
+        elif "state" in self.path:
+            self.show_status()
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+    def show_status(self):
+        statusList = []
+        for i, val in enumerate(pinlist):
+            pinState = GPIO.input(val)
+            statusList.append({'state': not pinState, 'pin': val})
 
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/json")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>Christmas controller</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>"+message+"</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self.wfile.write(bytes(json.dumps(statusList, indent=3), "utf-8"))
+
+    def write_log(self, state):
+        today = datetime.now()
+        fileformat  = today.strftime("%Y_%m_%d")+".log"
+        logFile = open("/var/log/christmas_server/"+fileformat, "a+")
+        timestamp = today.strftime("%H:%M:%S.%f")
+        logFile.write(timestamp+ ": Set lights to code "+state+"\r\n")
+
 if __name__ == "__main__":
     try:
         HTTPServer(("0.0.0.0", 1224), SimpleHandler).serve_forever()
